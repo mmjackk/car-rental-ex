2cf989a11d2221d8bdb35d9bc9aab591ab28c25e
Jacky Maman <mmjackk@gmail.com>
2014-11-29 18:06:48 +0200

    removing unnecessary constructors

--- src/administration/Car.java
+++ src/administration/Car.java
@@ -11,131 +11,13 @@ public class Car
 	private FuelType _fuelType;
 	private CarType _carType;
 	private int _pricePerDay;
-	
 	private boolean _availability = true;
 	
 
-	
-	public Car(String licenseNum, int topSpeed, int numSeats, FuelType fuelType, CarType carType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this._numSeats = numSeats;
-		this.setFuelType(fuelType);
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, int numSeats, FuelType fuelType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this._numSeats = numSeats;
-		this.setFuelType(fuelType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, int numSeats, CarType carType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this._numSeats = numSeats;
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, int numSeats, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this._numSeats = numSeats;
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, FuelType fuelType, CarType carType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this.setFuelType(fuelType);
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, FuelType fuelType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this.setFuelType(fuelType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int topSpeed, CarType carType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this._topSpeed = topSpeed;
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, FuelType fuelType, CarType carType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this.setFuelType(fuelType);
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, FuelType fuelType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this.setFuelType(fuelType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(String licenseNum, CarType carType, int pricePerDay) 
-	{
-		this._licenseNum = licenseNum;
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(FuelType fuelType, CarType carType, int pricePerDay) 
-	{
-		this.setFuelType(fuelType);
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(FuelType fuelType, int pricePerDay) 
-	{
-		this.setFuelType(fuelType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
-	public Car(CarType carType, int pricePerDay) 
-	{
-		this.setCarType(carType);
-		this.setPricePerDay(pricePerDay);
-		this._availability = true;
-	}
 	public Car() 
 	{
-		this._availability = true;
+		
 	}
-	
 	
 	
 	public String getLicenseNum() 
